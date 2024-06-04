a2153d3d90232d0af4c1e7c7a736792b7e038329
Marcos Longo <marcosv.longo@gmail.com>
2024-06-03 23:39:21 -0300

    wip: página de produtos

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -8,18 +8,27 @@ export default async function Produtos() {
     return (
         <main className="py-40">
             <div className="container">
-                <div className="grid grid-cols-3 gap-5">
-                    {produtos.map((item, index) =>
-                        <div key={index}>
-                            {item.node.title}
-                            <Image
-                                alt={item.node.title}
-                                src={item.node.produtos.imageDoProduto.node.mediaItemUrl}
-                                width={200}
-                                height={200}
-                            />
-                        </div>
-                    )}
+                <div className="mb-14">
+                    <h1 className="font-bold text-4xl">Escolha  a sua Bateria Ideal</h1>
+                </div>
+                <div className="flex gap-8">
+                    <aside className="w-1/4">
+                        filtros de busca
+                    </aside>
+                    <div className="w-3/4 grid grid-cols-3 gap-8">
+                        {produtos.map((item, index) =>
+                            <div key={index}>
+                                {item.node.title}
+                                <div className="relative w-[80%] h-[160px]">
+                                    <Image
+                                        alt={item.node.title}
+                                        src={item.node.produtos.imageDoProduto.node.mediaItemUrl}
+                                        fill
+                                    />
+                                </div>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </main>
