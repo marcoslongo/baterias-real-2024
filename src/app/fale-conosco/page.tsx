57d1d28f663bc87311d91c4aaf8c28e90930aa69
Marcos Longo <marcosv.longo@gmail.com>
2024-07-09 15:39:53 -0300

    refactor: ajusta página de contato

--- src/app/fale-conosco/page.tsx
+++ src/app/fale-conosco/page.tsx
@@ -3,34 +3,29 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Link from "next/link";
 import { IoLocationOutline, IoPhonePortraitOutline, IoMailOutline } from "react-icons/io5";
+import Image from "next/image";
 
 export default function FaleConosco() {
 	return (
-		<main className="py-40">
-			<div className="container flex flex-col items-center gap-10">
-				<div className="flex flex-col gap-2 w-full">
-					<h1 className="font-bold text-4xl">Fale conosco</h1>
-					<p>Tem alguma dúvida? Por favor utilize nossos meios e fale conosco. Temos a certeza que poderá receber a nossa resposta o mais rapidamente possível.</p>
-				</div>
-				<div className="flex gap-6 w-full">
-					<div className="w-2/3 flex flex-col gap-4 bg-white shadow-md p-9 rounded-md justify-center">
-						<div className="grid grid-cols-2 gap-3">
-							<Input type="text" placeholder="Nome" />
-							<Input type="text" placeholder="Sobrenome" />
-						</div>
-						<div>
-							<Input type="email" placeholder="E-mail" />
-						</div>
-						<div>
-							<Input type="phone" placeholder="Celular" />
-						</div>
-						<div>
-							<Textarea placeholder="Digite sua mensagem" />
-						</div>
-						<div className="w-full flex">
-							<Button className="w-full bg-[#DF0209] hover:bg-[#A60004]">Enviar Mensagem</Button>
-						</div>
+		<main className="pt-24 pb-40">
+			<div className="relative w-full min-h-[525px] mb-10 flex items-center">
+				<div className="container relative z-20">
+					<div className="w-1/3 flex flex-col gap-4">
+						<h1 className="font-bold text-6xl">Fale Conosco</h1>
+						<p className="text-xl">
+							Entre em contato conosco para <span className="font-bold text-[#DF0209]">esclarecer dúvidas, obter informações</span> e contribuir para a melhoria contínua dos nossos produtos.
+						</p>
 					</div>
+				</div>
+				<Image
+					src={'/assets/images/contact-desktop.webp'}
+					fill
+					objectFit="cover"
+					alt="banner contato"
+				/>
+			</div>
+			<div className="container flex flex-col items-center gap-10">
+				<div className="flex gap-6 w-full">
 					<div className="w-1/3 flex flex-col gap-4">
 						<CardInfo
 							name={"Telefone"}
@@ -47,6 +42,24 @@ export default function FaleConosco() {
 							info={"atendimento@bateriasreal.com.br"}
 							icon={<IoMailOutline />}
 						/>
+					</div>
+					<div className="w-2/3 flex flex-col gap-4 bg-white shadow-md p-9 rounded-md justify-center">
+						<div className="grid grid-cols-2 gap-3">
+							<Input className="h-12 text-base" type="text" placeholder="Nome" />
+							<Input className="h-12 text-base" type="text" placeholder="Sobrenome" />
+						</div>
+						<div>
+							<Input className="h-12 text-base" type="email" placeholder="E-mail" />
+						</div>
+						<div>
+							<Input className="h-12 text-base" type="phone" placeholder="Celular" />
+						</div>
+						<div>
+							<Textarea className="h-40 text-base" placeholder="Digite sua mensagem" />
+						</div>
+						<div className="w-full flex">
+							<Button className="w-full h-12 text-base font-bold bg-[#DF0209] hover:bg-[#A60004]">Enviar Mensagem</Button>
+						</div>
 					</div>
 				</div>
 			</div>
