a79c125ecad937de1c196ea60990971b9f7d7c9a
Marcos Longo <marcosv.longo@gmail.com>
2024-07-25 17:05:26 -0300

    wip

--- src/app/produtos/[slug]/page.tsx
+++ src/app/produtos/[slug]/page.tsx
@@ -32,10 +32,10 @@ export default async function PageProdutos({ params: { slug } }: PageProdutosPro
 
 	return (
 		<main>
-			<section className="relative h-[64vh] w-full">
+			<section className="relative w-full mt-10 mb-8 lg:mb-0">
 				<div className="container h-full">
-					<div className="relative z-30 grid grid-cols-2 gap-8 h-full items-center">
-						<div className="relative h-[450px]">
+					<div className="relative z-30 grid grid-cols-1 lg:grid-cols-2 gap-8 h-full items-center">
+						<div className="relative h-[250px] md:h-[350px] lg:h-[450px]">
 							<Image
 								src={banner.linhas.imagemBannerInterno.node.mediaItemUrl}
 								alt={banner.name}
@@ -44,7 +44,7 @@ export default async function PageProdutos({ params: { slug } }: PageProdutosPro
 							/>
 						</div>
 						<div className="flex flex-col gap-5 relative">
-							<h1 className="font-bold text-4xl uppercase">LINHA <br /> <span className="text-7xl text-[#DF0209] italic">{banner.name}</span></h1>
+							<h1 className="font-bold text-2xl md:text-3xl lg:text-4xl uppercase">LINHA <br /> <span className="text-3xl md:text-5xl lg:text-7xl text-[#DF0209] italic">{banner.name}</span></h1>
 							<p>{banner.linhas.textoSobreALinhaDeProdutos}</p>
 							<Image
 								src={'/assets/images/raio-vector.webp'}
@@ -58,11 +58,11 @@ export default async function PageProdutos({ params: { slug } }: PageProdutosPro
 			</section>
 			<section className="mb-24">
 				<div className="container">
-					<h2 className="text-5xl font-bold pb-6 flex gap-2 justify-center">
+					<h2 className="text-3xl md:text-4xl lg:text-5xl font-bold pb-6 flex gap-2 justify-center">
 						Baterias da linha
 					</h2>
 				</div>
-				<div className="container grid grid-cols-3 gap-8">
+				<div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 					{produtos.map((produto: any) => (
 						<Card
 							key={produto.node.id}
