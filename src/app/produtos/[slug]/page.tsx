ce3412460140d28f1d2beb85f24cb6d8c50d7d04
Marcos Longo <marcosv.longo@gmail.com>
2024-07-25 17:27:44 -0300

    wip

--- src/app/produtos/[slug]/page.tsx
+++ src/app/produtos/[slug]/page.tsx
@@ -35,7 +35,7 @@ export default async function PageProdutos({ params: { slug } }: PageProdutosPro
 			<section className="relative w-full mt-10 mb-8 lg:mb-0">
 				<div className="container h-full">
 					<div className="relative z-30 grid grid-cols-1 lg:grid-cols-2 gap-8 h-full items-center">
-						<div className="relative h-[260px] lg:h-[450px]">
+						<div className="relative h-[200px] lg:h-[450px]">
 							<Image
 								src={banner.linhas.imagemBannerInterno.node.mediaItemUrl}
 								alt={banner.name}
