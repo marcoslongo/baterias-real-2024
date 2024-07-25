be824d2e0db440b09e247419f6fdd8b063d8b970
Marcos Longo <marcosv.longo@gmail.com>
2024-07-25 18:16:18 -0300

    feat: desenvolve menu mobile

--- src/components/Header/Menumobile.tsx
+++ src/components/Header/Menumobile.tsx
@@ -1,7 +1,56 @@
-export function MenuMobile() {
-	return (
-		<nav>
+import { FaChevronDown } from "react-icons/fa6";
 
-		</nav>
+interface MenuMobileProps {
+	isOpen: boolean;
+}
+
+
+export function MenuMobile({ isOpen }: MenuMobileProps) {
+	return (
+		<div
+			className={`fixed w-full h-full bg-white z-50 transform transition-transform duration-300 ${isOpen ? "translate-x-0" : "translate-x-full"
+				}`}
+		>
+			<nav className="p-8">
+				<ul className="flex flex-col gap-3">
+					<MenuItem href={"/institucional"} title={"Sobre a Real"} />
+					<MenuItem href={"/representantes"} title={"Representantes"} />
+					<li className="flex gap-2 items-center">Produtos<FaChevronDown size={12} className="text-[#DF0209]" /></li>
+					<ul className="ml-6 flex flex-col gap-2">
+						<MenuItem href="/produtos/evolution" title="Linha Evolution" />
+						<MenuItem href="/produtos/linha-gold" title="Linha Gold" />
+						<MenuItem href="/produtos/linha-efb" title="Linha EFB" />
+						<MenuItem href="/produtos/linha-convencional" title="Linha Convencional" />
+						<MenuItem href="/produtos/linha-ecologica" title="Linha Ecológica" />
+						<MenuItem href="/produtos/linha-free" title="Linha Free" />
+					</ul>
+					<li className="flex gap-2 items-center">Downloads<FaChevronDown size={12} className="text-[#DF0209]" /></li>
+					<ul className="ml-6 flex flex-col gap-2">
+						<MenuItem href="/midias/feed" title="Posts para o feed" />
+						<MenuItem href="/midias/story" title="Posts para os Storys" />
+					</ul>
+					<li className="flex gap-2 items-center">Contatos<FaChevronDown size={12} className="text-[#DF0209]" /></li>
+					<ul className="ml-6 flex flex-col gap-2">
+						<MenuItem href="/fale-conosco" title="Fale conosco" />
+						<MenuItem href="/trabalhe-conosco" title="Trabalhe conosco" />
+					</ul>
+				</ul>
+			</nav>
+		</div>
+	);
+}
+
+interface MenuItemProps {
+	href: string;
+	title: string;
+}
+
+
+
+function MenuItem({ title, href }: MenuItemProps) {
+	return (
+		<li>
+			<a href={href}>{title}</a>
+		</li>
 	);
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,27 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Menu } from "./Menu";
+import { IoCloseOutline, IoMenuOutline } from "react-icons/io5";
+import { useState } from "react";
 import { MenuMobile } from "./Menumobile";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
+      <div onClick={toggleNav} className="flex lg:hidden text-black absolute right-5 top-10 z-[60]">
+        {isOpen ? (
+          <IoCloseOutline size={40} />
+        ) : (
+          <IoMenuOutline size={40} />
+        )}
+      </div>
+      <MenuMobile isOpen={isOpen} />
       <header className="w-full bg-white shadow-md relative z-40">
         <div className="container flex justify-between items-center py-7">
           <Link href="/">
@@ -18,7 +34,6 @@ export function Header() {
               priority
             />
           </Link>
-          <MenuMobile />
           <Menu />
         </div>
       </header>
