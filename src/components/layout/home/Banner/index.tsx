483ec59bc89903a2c7cfa1ba8bc38d1b23d7a317
Marcos Longo <marcosv.longo@gmail.com>
2024-07-29 20:39:05 -0300

    wip

--- src/components/layout/home/Banner/index.tsx
+++ src/components/layout/home/Banner/index.tsx
@@ -1,14 +1,17 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import { BsLightningChargeFill } from "react-icons/bs";
+import { useWindowSize } from "@/hooks/useWindowSize"
 
 export function Banner() {
+  const { width } = useWindowSize();
 
   return (
-    <section className="relative h-[900px] md:h-[670px] w-full">
-      <div className="container h-full flex items-start md:items-center">
-        <div className="w-full lg:w-3/5 relative z-30 flex gap-6 flex-col items-center mt-12 md:mt-0">
-          <h2 className="text-5xl text-white lg:text-black md:text-6xl text-center md:text-left font-bold leading-none mb-14">
+    <section className="relative h-auto lg:h-[670px] w-full">
+      <div className="container h-full flex items-start lg:items-center">
+        <div className="w-full lg:w-3/5 relative z-30 flex gap-6 flex-col items-center lg:items-start mt-12 lg:mt-0">
+          <h2 className="text-5xl text-white lg:text-black lg:text-6xl text-center lg:text-left font-bold leading-none">
             Para longas jornadas, muito mais
             <span className="text-[#E7C000] lg:text-[#DF0209]"> energia</span>
           </h2>
@@ -18,9 +21,9 @@ export function Banner() {
             alt="Banner home mobile"
             priority
             src={"/assets/images/banner-mobile.webp"}
-            className="flex md:hidden"
+            className="flex lg:hidden sm:mb-12 mt-12"
           />
-          <p className="text-sm lg:text-base text-center md:text-left md:pr-12">
+          <p className="text-sm lg:text-base text-center lg:text-left lg:pr-12">
             Com 44 anos de história, a Baterias Real oferece qualidade inigualável e energia duradoura para todos os desafios. Nossas linhas de baterias leves e pesadas são desenvolvidas para atender cada necessidade com a solução perfeita em acumuladores de energia automotiva. Seja para veículos leves ou para os maiores desafios em transporte e maquinário, a Baterias Real está ao seu lado com tecnologia de ponta e desempenho superior.
           </p>
           <h1 className="text-xl font-bold text-center md:text-left">
@@ -37,24 +40,24 @@ export function Banner() {
       <Image
         src={"/assets/images/baterias-home.webp"}
         alt="Banner home"
-        width={800}
+        width={width > 1441 ? (800) : (600)}
         height={500}
-        className="hidden md:block absolute right-0 z-20 top-40"
+        className="hidden lg:block absolute right-0 z-20 top-40"
         priority
       />
 
       <div className="w-full h-[480px] absolute top-0 bg-[#A60004] rounded-b-[280px] shadow-sm">
-        <Image          
+        <Image
           src={"/assets/images/raio-line.webp"}
           alt="alt"
           priority
           width={220}
           height={288}
-          className="absolute right-[50px] top-8"
+          className="absolute right-0 top-8"
         />
       </div>
       <Image
-        className="w-full h-full absolute z-10 hidden md:block"
+        className="w-full h-full absolute z-10 hidden lg:block"
         src={"/assets/images/bg-banner-home.webp"}
         alt="alt"
         fill
