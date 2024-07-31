9e7cd3e5dcc8ce529717d9349698d19cf84f4441
Marcos Longo <marcosv.longo@gmail.com>
2024-07-30 21:14:26 -0300

    refactor: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,30 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaPhone, FaLocationDot } from "react-icons/fa6";
+import { FaPhone, FaLocationDot, FaFacebookF, FaWhatsapp, FaInstagram } from "react-icons/fa6";
 
 export function Footer() {
   return (
     <footer className="bg-[#A60004] py-10 relative overflow-hidden">
       <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-12">
-        <div className="flex flex-col gap-6">
-          <Image
-            src={"/assets/images/logo-negativa.svg"}
-            alt="alt"
-            width={134}
-            height={250}
-          />
-          <h2 className="text-white">
-            © Baterias Real LTDA - 2024 Todos os Direitos Reservados
-          </h2>
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-6">
+            <Image
+              src={"/assets/images/logo-negativa.svg"}
+              alt="alt"
+              width={134}
+              height={250}
+            />
+            <h2 className="text-white">
+              © Baterias Real LTDA - 2024 Todos os Direitos Reservados
+            </h2>
+          </div>
+          <div className="flex flex-col gap-3">
+            <Link target="_blank" className="text-[#A60004] transition hover:scale-105 bg-white rounded-full w-10 h-10 flex items-center justify-center" href={'https://www.facebook.com/bateriasrealltda'}><FaFacebookF size={22} /></Link>
+            <Link target="_blank" className="text-[#A60004] transition hover:scale-105 bg-white rounded-full w-10 h-10 flex items-center justify-center" href={'https://www.instagram.com/bateriasreal/'}><FaInstagram size={22} /></Link>
+            <Link target="_blank" className="text-[#A60004] transition hover:scale-105 bg-white rounded-full w-10 h-10 flex items-center justify-center" href={'https://wa.me/5546984073435'}><FaWhatsapp size={22} /></Link>
+          </div>
         </div>
         <div className="flex flex-col gap-2 text-white">
           <h2 className="font-semibold text-2xl">Contato</h2>
           <div className="flex flex-col gap-6">
-            <Link className="flex items-center gap-2 hover:underline" href="href">
+            <Link className="flex items-center gap-2 hover:underline" href="tel:+554635431178">
               <FaPhone className="flex" size={16} />
               (46) 3543-1178
             </Link>
-            <Link className="flex flex-wrap items-center gap-2 hover:underline" href="href">
+            <Link className="flex flex-wrap items-center gap-2 hover:underline" target="_blank" href="https://maps.app.goo.gl/VXVXL5kWCowtwWTJ8">
               <div>
                 <FaLocationDot size={16} />
               </div>
