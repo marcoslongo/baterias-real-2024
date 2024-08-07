c5cd82494afe55409812912814b8b97217866e43
Marcos Longo <marcosv.longo@gmail.com>
2024-08-06 22:54:34 -0300

    wip

--- src/components/layout/home/ProdutosHome/Card.tsx
+++ src/components/layout/home/ProdutosHome/Card.tsx
@@ -4,30 +4,26 @@ import Link from "next/link";
 interface CardProps {
   name: string;
   imageBatery: string;
-  bgBatery: string;
   href: string
 }
 
-export function Card({ name, imageBatery, bgBatery, href }: CardProps) {
+export function Card({ name, imageBatery, href }: CardProps) {
   return (
-    <Link href={href} className="w-full h-[400px] flex relative transition hover:scale-105">
-      <div className="relative z-10 text-white pt-4 pl-10 flex flex-col justify-center">
-        <Image
-          src={imageBatery}
-          alt={name}
-          width={150}
-          height={75}
-        />
-        <h2 className="font-semibold text-2xl">
-          LINHA <br /> <span className="italic text-[2rem]">{name}</span>
+    <div>
+      <Link href={href} className="w-full flex flex-col transition">
+        <div className="text-white flex flex-col justify-center items-center">
+          <Image
+            src={imageBatery}
+            alt={name}
+            width={250}
+            height={75}
+          />
+
+        </div>
+        <h2 className="font-semibold text-xl text-black">
+          linha <br /> <span className="text-[2rem] font-extrabold uppercase">{name}</span>
         </h2>
-      </div>
-      <Image
-        src={bgBatery}
-        alt={name}
-        fill
-        className="rounded-2xl transition shadow-lg hover:shadow-xl object-cover"
-      />
-    </Link>
+      </Link>
+    </div>
   );
 }
