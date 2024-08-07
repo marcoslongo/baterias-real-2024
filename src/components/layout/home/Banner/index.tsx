3edae6cf017cfb3731c1ab0cde760f2a40b67f0a
Marcos Longo <marcosv.longo@gmail.com>
2024-08-06 22:09:02 -0300

    fix: corrige comflitos

--- src/components/layout/home/Banner/index.tsx
+++ src/components/layout/home/Banner/index.tsx
@@ -1,18 +1,10 @@
-'use client'
-import Image from "next/image";
-import Link from "next/link";
-import { BsLightningChargeFill } from "react-icons/bs";
-import { useWindowSize } from "@/hooks/useWindowSize"
+import { TextFade } from "./TextFade";
+import VideoBackground from "./VideoBackground";
 
 export function Banner() {
-  const { width } = useWindowSize();
-
   return (
-    <div className='h-[630px] relative overflow-hidden flex items-center justify-center'>
-      <h1 className='z-30 text-white text-6xl font-bold text-center'>
-        Para longas jornadas, <br /> muito mais <span className='text-[#A60004]'>energia</span>
-      </h1>
-      <div className='bg-black w-full h-full top-0 left-0 absolute z-20 opacity-90'></div>
+    <div className="h-[630px] overflow-hidden relative flex justify-center items-center">
+      <TextFade />
       <VideoBackground />
     </div>
   );
