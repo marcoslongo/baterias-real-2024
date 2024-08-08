7e49dd132d6d132ad03db7681c70ece63eccb170
Marcos Longo <marcosv.longo@gmail.com>
2024-08-07 23:10:22 -0300

    feat

--- src/components/layout/home/Banner/index.tsx
+++ src/components/layout/home/Banner/index.tsx
@@ -3,7 +3,7 @@ import VideoBackground from "./VideoBackground";
 
 export function Banner() {
   return (
-    <div className="h-[630px] overflow-hidden relative flex justify-center items-center">
+    <div className="h-[80vh] overflow-hidden relative flex justify-center items-center">
       <TextFade />
       <VideoBackground />
       <div className="w-full h-full absolute top-0 right-0 bg-black opacity-95 z-20 block"></div>
