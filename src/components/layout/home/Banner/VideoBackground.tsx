e334c66e7e55e7e058d57605e4bfd28a80d1376b
Marcos Longo <marcosv.longo@gmail.com>
2024-08-07 23:10:22 -0300

    feat

--- src/components/layout/home/Banner/VideoBackground.tsx
+++ src/components/layout/home/Banner/VideoBackground.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 
 const VideoBackground = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const videoStyle = {
+    transform: `translateY(${scrollY * 0.5}px)`,
+  };
+
   return (
-    <video className='w-full h-full object-cover absolute top-0 left-0' autoPlay muted loop>
-      <source src={'/assets/videos/video-banner.mp4'} type="video/mp4" />
+    <video
+      className="w-full h-full object-cover absolute top-0 left-0"
+      autoPlay
+      muted
+      loop
+      style={videoStyle}
+    >
+      <source src="/assets/videos/video-banner.mp4" type="video/mp4" />
     </video>
   );
 };
--- src/components/layout/home/ProdutosHome/Card.tsx
+++ src/components/layout/home/ProdutosHome/Card.tsx
@@ -9,14 +9,14 @@ interface CardProps {
 
 export function Card({ name, imageBatery, href }: CardProps) {
   return (
-    <div>
-      <Link href={href} className="w-full flex flex-col transition">
+    <div className="rounded-lg mt-4 relative hover:mt-[-10px] bg-white transition-all hover:shadow-lg">
+      <Link href={href} className="w-full flex flex-col relative bg-white p-4">
         <div className="text-white flex flex-col justify-center items-center">
           <Image
             src={imageBatery}
             alt={name}
-            width={250}
-            height={75}
+            width={300}
+            height={250}
           />
 
         </div>
