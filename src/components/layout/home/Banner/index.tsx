362af02912a7573d6d9b2580885f02eb0661ed8f
Marcos Longo <marcosv.longo@gmail.com>
2024-08-24 15:40:11 -0300

    refactor: ajusta opacidade do banner

--- src/components/layout/home/Banner/index.tsx
+++ src/components/layout/home/Banner/index.tsx
@@ -7,7 +7,7 @@ export function Banner() {
     <div className="h-[80vh] overflow-hidden relative flex justify-center items-center">
       <TextFade />
       <VideoBackground />
-      <div className="w-full h-full absolute top-0 right-0 bg-black opacity-95 z-20 block"></div>
+      <div className="w-full h-full absolute top-0 right-0 bg-black opacity-85 z-20 block"></div>
     </div>
   );
 }
