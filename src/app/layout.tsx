2c84e49a8a3623c41dcda60a4d43bf3a32f7125d
Marcos Longo <marcosv.longo@gmail.com>
2024-08-24 15:58:30 -0300

    feat: adc facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { Cookies } from '@/components/CookieConsent';
-
+import Script from 'next/script';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
 
@@ -24,7 +24,32 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
-        <Cookies/>
+        <Cookies />
+
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s) {
+                if(f.fbq)return;n=f.fbq=function(){
+                  n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+                };
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '783046172459225');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=783046172459225&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
