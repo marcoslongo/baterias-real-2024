f1a21ba050c4e089cadebd81075a3688a537ee39
Marcos Longo <marcosv.longo@gmail.com>
2025-02-04 22:32:05 -0300

    fix: ajusta form

--- src/app/seja-um-revendedor/Form/index.tsx
+++ src/app/seja-um-revendedor/Form/index.tsx
@@ -55,7 +55,7 @@ export function Form() {
 
 			console.log('Enviando dados:', templateParams);
 
-			const response = await emailjs.send('service_kni5y6p', 'template_f5x28ki', templateParams, 'SXExDJzUQWHDny-mu');
+			const response = await emailjs.send('service_cj9spuz', 'template_qr3vhzq', templateParams, 'Nme405_wJLJl-HLvn');
 
 			if (response.status !== 200) {
 				throw new Error('Erro ao enviar o e-mail. Status: ' + response.status);
